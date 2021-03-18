40903b0b8a8e123617b1abfa5adc3e27a4c95f33
glauberrleite <contact@glauberrleite.com>
2021-03-18 20:40:39 -0300

    working robot_fb

--- reactive_robot.m
+++ reactive_robot.m
@@ -1,8 +1,11 @@
+clc
+clear all
+
 import matlab.net.*
 import matlab.net.http.*
 import matlab.net.http.field.*
 
-% Retrieve kde and env_sensibility from server API
+% Retrieve kde, env_sensibility and robots positions/heading from server API
 r = RequestMessage;
 uri = URI('http://localhost:5000/kde/');
 resp = send(r,uri);
@@ -11,43 +14,58 @@ kde = resp.Body.Data.kde;
 r = RequestMessage;
 uri = URI('http://localhost:5000/env_sensibility/');
 resp = send(r,uri);
-env_sensibility = resp.Body.Data.env_sensibility;
+dist_grid = resp.Body.Data.env_sensibility;
 
+r = RequestMessage;
+uri = URI('http://localhost:5000/robots_pos/');
+resp = send(r,uri);
+robots_pos = resp.Body.Data.robots_pos;
+heading = resp.Body.Data.robots_heading;
 
+aux_mask = kde < 0;
 
-for robot = 1:n_robots
-    neighbors = robots(setdiff(1:end, robot), :);
-    target = computeTargetMulti(robots(robot, :), heading(robot), grid, neighbors, dist_grid,robot, weights(robot,:));
-    if norm(target - robots(robot, :)) > 0
-        % A*
-        GoalRegister = int8(zeros(size(mask)));
-        GoalRegister(target(2), target(1)) = 1;
-        for k = 1:size(neighbors, 1)
-           aux_mask(neighbors(k, 2), neighbors(k, 1)) = 1;
-        end
-        result = ASTARPATH(robots(robot, 1), robots(robot, 2), aux_mask, GoalRegister, 1);
-        if size(result, 1) > 1
-            move = [result(end - 1, 2) - robots(robot, 1), result(end - 1, 1) - robots(robot, 2)];
+weights = [2.0, 0.1, 0.3, 0.2, 1];
+robot_id = 0;
 
-            robots(robot, :) = robots(robot, :) + move;
-            heading(robot) = atan2(move(2), move(1));
-        end
-    else 
-        disp(['Robot ', num2str(robot), ' stopped']);
+% Matlab's different indexing starts with 1 instead of 0
+robot = robot_id + 1;
+robots_pos = robots_pos + 1;
+
+neighbors = robots_pos(setdiff(1:end, robot), :);
+target = computeTargetMulti(robots_pos(robot, :), heading(robot), kde, neighbors, dist_grid, weights);
+if norm(target - robots_pos(robot, :)) > 0
+    % A*
+    GoalRegister = int8(zeros(size(aux_mask)));
+    GoalRegister(target(2), target(1)) = 1;
+    for k = 1:size(neighbors, 1)
+       aux_mask(neighbors(k, 2), neighbors(k, 1)) = 1;
     end
+    result = ASTARPATH(robots_pos(robot, 1), robots_pos(robot, 2), aux_mask, GoalRegister, 1);
+    if size(result, 1) > 1
+        move = [result(end - 1, 2) - robots_pos(robot, 1), result(end - 1, 1) - robots_pos(robot, 2)];
+
+        robots_pos(robot, :) = robots_pos(robot, :) + move;
+        heading(robot) = atan2(move(2), move(1));
+    end
+else 
+    disp(['Robot ', num2str(robot), ' stopped']);
 end
 
 
-% Publish robot computed position to server API
+% Publish robot computed position and heading to server API
+xgrid = robots_pos(robot, 1) - 1;
+ygrid = robots_pos(robot, 2) - 1;
+request_str = ['{"robot_id": "', num2str(robot_id), '", "xgrid": "', num2str(xgrid), '", "ygrid": "', num2str(ygrid), '", "robot_heading": "', num2str(heading(robot)) , '", "lon": "[]", "lat": "[]"}'];
 request = RequestMessage( 'POST', ...
     [ContentTypeField( 'application/vnd.api+json' ), AcceptField('application/vnd.api+json')], ...
-    '{"xgrid": "21", "ygrid": "38", "lon": "[]", "lat": "[]"}' );
+     request_str);
 response = request.send('http://localhost:5000/robot_fb/');
 
-function target = computeTargetMulti(pos, heading, grid, neighbors, dist_grid,robot,weights)
+function target = computeTargetMulti(pos, heading, grid, neighbors, dist_grid, weights)
     max_value = 0;
     target = pos;
     max_heading = 0;
+    mapvalue = zeros(size(grid));
     
     omega_c = weights(1);
     omega_s = weights(2);
@@ -75,9 +93,9 @@ function target = computeTargetMulti(pos, heading, grid, neighbors, dist_grid,ro
                     end
                     
                     if grid(j, i)>0
-                        mapvalue(j,i) =max(kappa + omega_c*grid(j, i) + omega_s*dist_grid(j ,i) - omega_d * distance + omega_n * distance_nearest_neigh, 0);
+                        mapvalue(j,i) = max(kappa + omega_c*grid(j, i) + omega_s*dist_grid(j ,i) - omega_d * distance + omega_n * distance_nearest_neigh, 0);
                     else
-                        mapvalue(j,i)=0;
+                        mapvalue(j,i) = 0;
                     end
 
                     value = mapvalue(j,i);
--- test_api_robot_fb.m
+++ test_api_robot_fb.m
@@ -1,6 +1,8 @@
 import matlab.net.http.*
 import matlab.net.http.field.*
+
+request_str = '{"robot_id": "0", "xgrid": "25", "ygrid": "36", "robot_heading": "0", "lon": "[]", "lat": "[]"}';
 request = RequestMessage( 'POST', ...
     [ContentTypeField( 'application/vnd.api+json' ), AcceptField('application/vnd.api+json')], ...
-    '{"xgrid": "25", "ygrid": "36", "lon": "[]", "lat": "[]"}' );
+     request_str);
 response = request.send('http://localhost:5000/robot_fb/');
